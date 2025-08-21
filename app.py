e6a22f5e9b8b92223ada2f658b4684b1d292ea66
chris1252 <cej29@students.uwf.edu>
2025-08-21 18:09:40 -0500

    formatted return statements

--- app.py
+++ app.py
@@ -18,26 +18,43 @@ except Exception as e:
 @app.route('/')
 def home():
     return jsonify({
+
         "service": "Loan Prediction API",
+
         "version": "0.1.0",
+
         "model_accuracy": ml_model.accuracy if ml_model else "N/A",
+
         "endpoints":{
             "predict": "/predict",
+
             "model_info": "/model-info",
+
             "health": "/health",
+
         },
-        
+
         "example_request":{
             "no_of_dependents": 1,
+
             "education": "Graduate",
+
             "self_employed": "no",
+
             "income_annum": 120000,
+
             "loan_ammount": 7000,
+
             "loan_term": 72,
+
             "cibil_score": 690,
+
             "residential_assets_value": 0,
+
             "commercial_assets_value": 0,
+
             "luxury_assets_value" : 0,
+
             "bank_asset_value": 0
         }
     })
@@ -45,7 +62,9 @@ def home():
 def health():
     return jsonify({
         "status": "healty" if ml_model else "unhealthy",
+
         "timestamp": datetime.now().isoformat(),
+
         "model_loaded": ml_model is not None
     })
 @app.route('/model-info')
@@ -55,10 +74,15 @@ def model_info():
     
     return jsonify({
         "model_type" : "Decision Tree",
+
         "accuracy": ml_model.accuracy,
+
         "features": ml_model.feature_names.tolist(), # needed to be tolist() bc it was ndarray
+
         "classes": ml_model.target_names.tolist(),
+
         "num_featues": len(ml_model.feature_names),
+
         "num_classes": len(ml_model.target_names)
 
     })
@@ -79,7 +103,9 @@ def predict():
 
         return jsonify({
             "status": "success",
+
             "prediction" : result,
+            
             "timestamp": datetime.now().isoformat()
         })
     except ValueError as v:
